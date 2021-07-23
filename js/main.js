9e17929757c0eaf5ea7845972ccaaded3e430f2a
Maxim Roschin <mroschin2@yandex.ru>
2021-07-23 23:55:26 +0300

    added sliders and footer

--- js/main.js
+++ js/main.js
@@ -39,23 +39,21 @@ const getElement = (tagName, classNames, attributes) => {
 };
 
 //header
-const createHeader = (params) => {
+const createHeader = ({ title, header: { logo, menu, social} }) => {
 	const header = getElement("header");
 	const container = getElement("div", ["container"]);
 	const wrapper = getElement("div", ["header"]);
-	if (params.header.logo) {
-		const logo = getElement("img", ["logo"], {
-            src: params.header.logo,
-            alt: "логотип " + params.title,
+	if (logo) {
+		const logoImg = getElement("img", ["logo"], {
+            src: logo,
+            alt: "логотип " + title,
         });
-		// logo.src = params.header.logo;
-		// logo.alt = "логотип " + params.title;
-		wrapper.append(logo);
+		wrapper.append(logoImg);
 	}
 	//меню
-	if (params.header.menu) {
+	if (menu) {
 		const menuWrapper = getElement("nav", ["menu-list"]);
-		const allMenu = params.header.menu.map((item) => {
+		const allMenu = menu.map((item) => {
 			const menuLink = getElement("a", ["menu-link"], {
 				href: item.link,
 				textContent: item.title
@@ -65,11 +63,19 @@ const createHeader = (params) => {
 		});
 		menuWrapper.append(...allMenu);
 		wrapper.append(menuWrapper);
+		//кнопка меню для малых экранов
+		const menuBtn = getElement("button", ["menu-button"]);
+        // console.log(menuBtn);
+        menuBtn.addEventListener("click", () => {
+            menuBtn.classList.toggle("menu-button-active");
+            wrapper.classList.toggle("header-active");
+        });
+        container.append(menuBtn);
 	}
 	//соцсети
-	if (params.header.social) {
+	if (social) {
 		const socialWrapper = getElement("div", ["social"]);
-		const allSocial = params.header.social.map((item) => {
+		const allSocial = social.map((item) => {
         	const socialLink = getElement("a", ["social-link"]);
         	socialLink.href = item.link;
         	// const socialImg = getElement("img");
@@ -85,12 +91,15 @@ const createHeader = (params) => {
 		wrapper.append(socialWrapper);
 	}
 
+
 	header.append(container);
 	container.append(wrapper);
 	return header;
 };
+
+
 //main section
-const createMain = ({ title, main: { genre, rating, description, trailer } }) => {
+const createMain = ({ title, main: { genre, rating, description, trailer, slider } }) => {
 	const main = getElement("main");
 	const container = getElement("div", ["container"]);
 	main.append(container);
@@ -157,12 +166,122 @@ const createMain = ({ title, main: { genre, rating, description, trailer } }) =>
 		wrapper.append(youtubeImgLink);
 	}
 
+	if (slider) {
+		const sliderBlock = getElement("div", ["series"]);
+		const swiperBlock = getElement("div", ["swiper-container"]);
+		const swiperWrapper = getElement("div", ["swiper-wrapper"]);
+		const arrow = getElement("button", ["arrow"]);
+
+		const allSliders = slider.map((item) => {
+			let alt = "";
+			
+			const swiperSlide = getElement("div", ["swiper-slide"]);
+			const swiperCard = getElement("figure", ["card"]);
+			swiperSlide.append(swiperCard);
+			//image
+			const swiperImg = getElement("img", ["card-img"], {
+				src: item.img,
+				alt: item.title + " " + item.subtitle
+			});
+			swiperCard.append(swiperImg);
+			//descr
+			if (item.title || item.subtitle) {
+				const swiperFigCaption = getElement("figcaption", ["card-description"]);
+				if (item.subtitle) {
+					const swiperSubtitle = getElement("p", ["card-subtitle"], {
+						textContent: item.subtitle
+					});
+					swiperFigCaption.append(swiperSubtitle);
+				}
+				if (item.title) {
+					const swiperTitle = getElement("p", ["card-title"], {
+						textContent: item.title
+					});
+					swiperFigCaption.append(swiperTitle);
+				}
+				swiperCard.append(swiperFigCaption);
+			}
+
+			return swiperSlide;
+		});
+		swiperWrapper.append(...allSliders);
+		swiperBlock.append(swiperWrapper);
+		sliderBlock.append(swiperBlock);
+		sliderBlock.append(arrow);
+		container.append(sliderBlock);
+		new Swiper(swiperBlock, {
+		    loop: true,
+		    navigation: {
+		        nextEl: arrow,
+		    },
+		    breakpoints: {
+        		320: {
+		            slidesPerView: 1,
+        		    spaceBetween: 20,
+		        },
+        		541: {
+		            slidesPerView: 2,
+        		    spaceBetween: 40,
+		        },
+    		},
+		});
+	}
+
 	return main;
+};
+
+//footer 
+const createFooter = ({ title, footer: { copyright, menu } }) => {
+	const footerBlock = getElement("footer", ["footer"]);
+	const container = getElement("div", ["container"]);
+	const content = getElement("div", ["footer-content"]);
+	container.append(content);
+	footerBlock.append(container);
+	if (copyright) {
+		const copyrightBlock = getElement("div", ["left"]);
+		const copyrightContent = getElement("span", ["copyright"], {
+			textContent: copyright
+		});
+		copyrightBlock.append(copyrightContent);
+		content.append(copyrightBlock);
+	}
+	if (menu) {
+		const menuBlock = getElement("div", ["right"]);
+		const menuNav = getElement("nav", ["footer-menu"]);
+		menuBlock.append(menuNav);
+		const allMenu = menu.map((item) => {
+			return getElement("a", ["footer-link"], {
+				textContent: item.title,
+				href: item.link
+			});
+		})
+		menuNav.append(...allMenu);
+		content.append(menuBlock);
+	}
+
+	return footerBlock;
 };
 
 const movieConstructor = (selector, options) => {
 	const app = document.querySelector(selector);
 	app.classList.add("body-app");
+
+	app.style.color = options.fontColor || "";
+	app.style.backgroundColor = options.backgroundColor || "";
+	if (options.subColor) {
+		document.documentElement.style.setProperty("--sub-color", options.subColor);
+	}
+
+	if (options.favicon) {
+		const index = options.favicon.lastIndexOf(".");
+		const type = options.favicon.substring(index + 1);
+		const favicon = getElement("link", null, {
+			rel: "icon",
+			href: options.favicon,
+			type: "image/" + type === "svg" ? "image/svg-xml" : type
+		});
+		document.head.append(favicon);
+	}
 
 	app.style.backgroundImage = options.background ?
 		`url("${options.background}")` : "";
@@ -176,12 +295,20 @@ const movieConstructor = (selector, options) => {
 	if (options.main) {
 		app.append(createMain(options));
 	}
+	//футер
+	if (options.footer) {
+		app.append(createFooter(options));
+	}
 };
 
 
 movieConstructor(".app", {
     title: "Ведьмак",
     background: "witcher/background.jpg",
+    favicon: "witcher/logo.png",
+    fontColor: "#ffffff",
+    backgroundColor: "#141218",
+    subColor: "#9D2929",
     header: {
         logo: "witcher/logo.png",
         social: [
@@ -223,5 +350,44 @@ movieConstructor(".app", {
                         путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от
                         всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.`,
         trailer: "https://www.youtube.com/watch?v=P0oJqfLzZzQ",
+        slider: [
+            {
+                img: "witcher/series/series-1.jpg",
+                title: "Начало конца",
+                subtitle: "Серия №1",
+            },
+            {
+                img: "witcher/series/series-2.jpg",
+                title: "Четыре марки",
+                subtitle: "Серия №2",
+            },
+            {
+                img: "witcher/series/series-3.jpg",
+                title: "Предательская луна",
+                subtitle: "Серия №3",
+            },
+            {
+                img: "witcher/series/series-4.jpg",
+                title: "Банкеты, ублюдки и похороны",
+                subtitle: "Серия №4",
+            },
+        ],
+    },
+    footer: {
+		copyright: "© 2020 The Witcher. All right reserved.",
+		menu: [
+			{
+				title: "Privacy Policy",
+				link: "#"
+			},
+			{
+				title: "Terms of Service",
+				link: "#"
+			},
+			{
+				title: "Legal",
+				link: "#"
+			}
+		]
     },
 });
