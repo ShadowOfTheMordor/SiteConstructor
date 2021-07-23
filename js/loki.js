377108b4dae5ac8195dbcae8b6acbeed7d2e05c8
Maxim Roschin <mroschin2@yandex.ru>
2021-07-24 01:26:49 +0300

    constructed version for loki series

---
+++ js/loki.js
@@ -0,0 +1,98 @@
+export default {
+    title: "Локи",
+    background: "loki/background.jpg",
+    favicon: "loki/logo.png",
+    fontColor: "#ffffff",
+    backgroundColor: "#000",
+    subColor: "#014206",
+    header: {
+        logo: "loki/logo.png",
+        social: [
+            {
+                title: "Twitter",
+                link: "https://twitter.com",
+                image: "loki/social/twitter.svg",
+            },
+            {
+                title: "Instagram",
+                link: "https://instagram.com",
+                image: "loki/social/instagram.svg",
+            },
+            {
+                title: "Facebok",
+                link: "https://facebook.com",
+                image: "loki/social/facebook.svg",
+            },
+        ],
+        menu: [
+            {
+                title: "Описание",
+                link: "#",
+            },
+            {
+                title: "Трейлер",
+                link: "#",
+            },
+            {
+                title: "Отзывы",
+                link: "#",
+            },
+        ],
+    },
+    main: {
+        genre: "2021, фантастика, фэнтези, боевик, приключения",
+        rating: "8",
+        description:
+            "Локи попадает в таинственную организацию «Управление временными изменениями» после того, как он украл Тессеракт, и путешествует во времени, меняя историю.",
+        trailer: "https://youtu.be/YrjHcYqe31g",
+        slider: [
+            {
+                img: "loki/series/series-1.jpg",
+                title: "Славная миссия",
+                subtitle: "Серия №1",
+            },
+            {
+                img: "loki/series/series-2.jpg",
+                title: "Вариант",
+                subtitle: "Серия №2",
+            },
+            {
+                img: "loki/series/series-3.jpg",
+                title: "Ламентис",
+                subtitle: "Серия №3",
+            },
+            {
+                img: "loki/series/series-4.jpg",
+                title: "Смежное событие",
+                subtitle: "Серия №4",
+            },
+            {
+                img: "loki/series/series-5.jpg",
+                title: "Путешествие в неизвестность",
+                subtitle: "Серия №5",
+            },
+            {
+                img: "loki/series/series-6.jpg",
+                title: "На все времена. Всегда",
+                subtitle: "Серия №6",
+            },
+        ],
+    },
+    footer: {
+        copyright: "© 2020 The Witcher. All right reserved.",
+        menu: [
+            {
+                title: "Privacy Policy",
+                link: "#",
+            },
+            {
+                title: "Terms of Service",
+                link: "#",
+            },
+            {
+                title: "Legal",
+                link: "#",
+            },
+        ],
+    },
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// import configObject from "./witcher.js";
+import configObject from "./loki.js";
+
 /*
 new Swiper('.swiper-container', {
 	loop: true,
@@ -23,371 +26,286 @@ menuButton.addEventListener('click', function () {
 	menu.classList.toggle('header-active');
 });
 */
+
 const getElement = (tagName, classNames, attributes) => {
-	//classNames - массив строк
-	const element = document.createElement(tagName);
-	if (classNames) {
-		element.classList.add(...classNames);
-	}
-	//аттрибуты
-	if (attributes) {
-		for (const attribute in attributes) {
-			element[attribute] = attributes[attribute];
-		}
-	}
-	return element;
+    //classNames - массив строк
+    const element = document.createElement(tagName);
+    if (classNames) {
+        element.classList.add(...classNames);
+    }
+    //аттрибуты
+    if (attributes) {
+        for (const attribute in attributes) {
+            element[attribute] = attributes[attribute];
+        }
+    }
+    return element;
 };
 
 //header
-const createHeader = ({ title, header: { logo, menu, social} }) => {
-	const header = getElement("header");
-	const container = getElement("div", ["container"]);
-	const wrapper = getElement("div", ["header"]);
-	if (logo) {
-		const logoImg = getElement("img", ["logo"], {
+const createHeader = ({ title, header: { logo, menu, social } }) => {
+    const header = getElement("header");
+    const container = getElement("div", ["container"]);
+    const wrapper = getElement("div", ["header"]);
+    if (logo) {
+        const logoImg = getElement("img", ["logo"], {
             src: logo,
             alt: "логотип " + title,
         });
-		wrapper.append(logoImg);
-	}
-	//меню
-	if (menu) {
-		const menuWrapper = getElement("nav", ["menu-list"]);
-		const allMenu = menu.map((item) => {
-			const menuLink = getElement("a", ["menu-link"], {
-				href: item.link,
-				textContent: item.title
-			});
-			// menuLink.textContent = item.title;
-			return menuLink;
-		});
-		menuWrapper.append(...allMenu);
-		wrapper.append(menuWrapper);
-		//кнопка меню для малых экранов
-		const menuBtn = getElement("button", ["menu-button"]);
+        wrapper.append(logoImg);
+    }
+    //меню
+    if (menu) {
+        const menuWrapper = getElement("nav", ["menu-list"]);
+        const allMenu = menu.map((item) => {
+            const menuLink = getElement("a", ["menu-link"], {
+                href: item.link,
+                textContent: item.title,
+            });
+            // menuLink.textContent = item.title;
+            return menuLink;
+        });
+        menuWrapper.append(...allMenu);
+        wrapper.append(menuWrapper);
+        //кнопка меню для малых экранов
+        const menuBtn = getElement("button", ["menu-button"]);
         // console.log(menuBtn);
         menuBtn.addEventListener("click", () => {
             menuBtn.classList.toggle("menu-button-active");
             wrapper.classList.toggle("header-active");
         });
         container.append(menuBtn);
-	}
-	//соцсети
-	if (social) {
-		const socialWrapper = getElement("div", ["social"]);
-		const allSocial = social.map((item) => {
-        	const socialLink = getElement("a", ["social-link"]);
-        	socialLink.href = item.link;
-        	// const socialImg = getElement("img");
+    }
+    //соцсети
+    if (social) {
+        const socialWrapper = getElement("div", ["social"]);
+        const allSocial = social.map((item) => {
+            const socialLink = getElement("a", ["social-link"]);
+            socialLink.href = item.link;
+            // const socialImg = getElement("img");
             // socialImg.src = item.image;
-        	// socialImg.alt = item.title;
-			socialLink.append(getElement("img", [], {
-				src: item.image,
-				alt: item.title
-			}));
-			return socialLink;
-		});
-		socialWrapper.append(...allSocial);
-		wrapper.append(socialWrapper);
-	}
+            // socialImg.alt = item.title;
+            socialLink.append(
+                getElement("img", [], {
+                    src: item.image,
+                    alt: item.title,
+                })
+            );
+            return socialLink;
+        });
+        socialWrapper.append(...allSocial);
+        wrapper.append(socialWrapper);
+    }
 
-
-	header.append(container);
-	container.append(wrapper);
-	return header;
+    header.append(container);
+    container.append(wrapper);
+    return header;
 };
-
 
 //main section
 const createMain = ({ title, main: { genre, rating, description, trailer, slider } }) => {
-	const main = getElement("main");
-	const container = getElement("div", ["container"]);
-	main.append(container);
-	const wrapper = getElement("div", ["main-content"]);
-	container.append(wrapper);
-	const content = getElement("div", ["content"]);
-	wrapper.append(content);
+    const main = getElement("main");
+    const container = getElement("div", ["container"]);
+    main.append(container);
+    const wrapper = getElement("div", ["main-content"]);
+    container.append(wrapper);
+    const content = getElement("div", ["content"]);
+    wrapper.append(content);
 
-	if (genre) {
-		const genreSpan = getElement("span", ["genre", "animated", "fadeInRight"], {
-			textContent: genre
-		});
-		content.append(genreSpan);
-	}
+    if (genre) {
+        const genreSpan = getElement("span", ["genre", "animated", "fadeInRight"], {
+            textContent: genre,
+        });
+        content.append(genreSpan);
+    }
 
-	//рейтинг со звездочками
-	if (rating) {
-		const ratingWrapper = getElement("div", ["rating", "animated", "fadeInRight"]);
-		const ratingStars = getElement("div", ["rating-stars"]);
-		const ratingNumber = getElement("div", ["rating-number"], {
+    //рейтинг со звездочками
+    if (rating) {
+        const ratingWrapper = getElement("div", ["rating", "animated", "fadeInRight"]);
+        const ratingStars = getElement("div", ["rating-stars"]);
+        const ratingNumber = getElement("div", ["rating-number"], {
             textContent: rating + "/10",
         });
-		ratingWrapper.append(ratingStars, ratingNumber);
-		//stars
-		const starsAlt = `Рейтинг ${rating} из 10`;
-		// listing
-		for (let i = 0; i < 10; i++) {
-			const star = getElement("img", ["star"], {
-				alt: i ? "" : `Рейтинг ${rating} из 10`,
-				src: i < rating ? "img/star.svg" : "img/star-o.svg"
-			});
-			ratingStars.append(star);
-		}
-		content.append(ratingWrapper);
-	}
+        ratingWrapper.append(ratingStars, ratingNumber);
+        //stars
+        const starsAlt = `Рейтинг ${rating} из 10`;
+        // listing
+        for (let i = 0; i < 10; i++) {
+            const star = getElement("img", ["star"], {
+                alt: i ? "" : `Рейтинг ${rating} из 10`,
+                src: i < rating ? "img/star.svg" : "img/star-o.svg",
+            });
+            ratingStars.append(star);
+        }
+        content.append(ratingWrapper);
+    }
 
-	//title
-	content.append(getElement("h1", ["main-title", "animated", "fadeInRight"], {
-		textContent: title
-	}));
-	//description
-	if (description) {
-		content.append(getElement("p", ["main-description", "animated", "fadeInRight"], {
-			textContent: description
-		}));
-	}
-	//trailer
-	if (trailer) {
-		const youtubeLink = getElement("a", ["button", "animated", "fadeInRight", "youtube-modal"], {
+    //title
+    content.append(
+        getElement("h1", ["main-title", "animated", "fadeInRight"], {
+            textContent: title,
+        })
+    );
+    //description
+    if (description) {
+        content.append(
+            getElement("p", ["main-description", "animated", "fadeInRight"], {
+                textContent: description,
+            })
+        );
+    }
+    //trailer
+    if (trailer) {
+        const youtubeLink = getElement("a", ["button", "animated", "fadeInRight", "youtube-modal"], {
             href: trailer,
             textContent: "Смотреть трейлер",
         });
-		content.append(youtubeLink);
-		const youtubeImgLink = getElement("a", ["play", "youtube-modal"], {
-			href: trailer,
-			ariaLabel: "Смотреть трейлер"
-		});
-		const iconPlay = getElement("img", ["play-img"], {
-			src: "img/play.svg",
-			alt: "",
-			ariaHidden: true
-		});
-		youtubeImgLink.append(iconPlay);
-		wrapper.append(youtubeImgLink);
-	}
+        content.append(youtubeLink);
+        const youtubeImgLink = getElement("a", ["play", "youtube-modal"], {
+            href: trailer,
+            ariaLabel: "Смотреть трейлер",
+        });
+        const iconPlay = getElement("img", ["play-img"], {
+            src: "img/play.svg",
+            alt: "",
+            ariaHidden: true,
+        });
+        youtubeImgLink.append(iconPlay);
+        wrapper.append(youtubeImgLink);
+    }
 
-	if (slider) {
-		const sliderBlock = getElement("div", ["series"]);
-		const swiperBlock = getElement("div", ["swiper-container"]);
-		const swiperWrapper = getElement("div", ["swiper-wrapper"]);
-		const arrow = getElement("button", ["arrow"]);
+    if (slider) {
+        const sliderBlock = getElement("div", ["series"]);
+        const swiperBlock = getElement("div", ["swiper-container"]);
+        const swiperWrapper = getElement("div", ["swiper-wrapper"]);
+        const arrow = getElement("button", ["arrow"]);
 
-		const allSliders = slider.map((item) => {
-			let alt = "";
-			
-			const swiperSlide = getElement("div", ["swiper-slide"]);
-			const swiperCard = getElement("figure", ["card"]);
-			swiperSlide.append(swiperCard);
-			//image
-			const swiperImg = getElement("img", ["card-img"], {
-				src: item.img,
-				alt: item.title + " " + item.subtitle
-			});
-			swiperCard.append(swiperImg);
-			//descr
-			if (item.title || item.subtitle) {
-				const swiperFigCaption = getElement("figcaption", ["card-description"]);
-				if (item.subtitle) {
-					const swiperSubtitle = getElement("p", ["card-subtitle"], {
-						textContent: item.subtitle
-					});
-					swiperFigCaption.append(swiperSubtitle);
-				}
-				if (item.title) {
-					const swiperTitle = getElement("p", ["card-title"], {
-						textContent: item.title
-					});
-					swiperFigCaption.append(swiperTitle);
-				}
-				swiperCard.append(swiperFigCaption);
-			}
+        const allSliders = slider.map((item) => {
+            let alt = "";
 
-			return swiperSlide;
-		});
-		swiperWrapper.append(...allSliders);
-		swiperBlock.append(swiperWrapper);
-		sliderBlock.append(swiperBlock);
-		sliderBlock.append(arrow);
-		container.append(sliderBlock);
-		new Swiper(swiperBlock, {
-		    loop: true,
-		    navigation: {
-		        nextEl: arrow,
-		    },
-		    breakpoints: {
-        		320: {
-		            slidesPerView: 1,
-        		    spaceBetween: 20,
-		        },
-        		541: {
-		            slidesPerView: 2,
-        		    spaceBetween: 40,
-		        },
-    		},
-		});
-	}
+            const swiperSlide = getElement("div", ["swiper-slide"]);
+            const swiperCard = getElement("figure", ["card"]);
+            swiperSlide.append(swiperCard);
+            //image
+            const swiperImg = getElement("img", ["card-img"], {
+                src: item.img,
+                alt: item.title + " " + item.subtitle,
+            });
+            swiperCard.append(swiperImg);
+            //descr
+            if (item.title || item.subtitle) {
+                const swiperFigCaption = getElement("figcaption", ["card-description"]);
+                if (item.subtitle) {
+                    const swiperSubtitle = getElement("p", ["card-subtitle"], {
+                        textContent: item.subtitle,
+                    });
+                    swiperFigCaption.append(swiperSubtitle);
+                }
+                if (item.title) {
+                    const swiperTitle = getElement("p", ["card-title"], {
+                        textContent: item.title,
+                    });
+                    swiperFigCaption.append(swiperTitle);
+                }
+                swiperCard.append(swiperFigCaption);
+            }
 
-	return main;
+            return swiperSlide;
+        });
+        swiperWrapper.append(...allSliders);
+        swiperBlock.append(swiperWrapper);
+        sliderBlock.append(swiperBlock);
+        sliderBlock.append(arrow);
+        container.append(sliderBlock);
+        new Swiper(swiperBlock, {
+            loop: true,
+            navigation: {
+                nextEl: arrow,
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                },
+                541: {
+                    slidesPerView: 2,
+                    spaceBetween: 40,
+                },
+            },
+        });
+    }
+
+    return main;
 };
 
-//footer 
+//footer
 const createFooter = ({ title, footer: { copyright, menu } }) => {
-	const footerBlock = getElement("footer", ["footer"]);
-	const container = getElement("div", ["container"]);
-	const content = getElement("div", ["footer-content"]);
-	container.append(content);
-	footerBlock.append(container);
-	if (copyright) {
-		const copyrightBlock = getElement("div", ["left"]);
-		const copyrightContent = getElement("span", ["copyright"], {
-			textContent: copyright
-		});
-		copyrightBlock.append(copyrightContent);
-		content.append(copyrightBlock);
-	}
-	if (menu) {
-		const menuBlock = getElement("div", ["right"]);
-		const menuNav = getElement("nav", ["footer-menu"]);
-		menuBlock.append(menuNav);
-		const allMenu = menu.map((item) => {
-			return getElement("a", ["footer-link"], {
-				textContent: item.title,
-				href: item.link
-			});
-		})
-		menuNav.append(...allMenu);
-		content.append(menuBlock);
-	}
+    const footerBlock = getElement("footer", ["footer"]);
+    const container = getElement("div", ["container"]);
+    const content = getElement("div", ["footer-content"]);
+    container.append(content);
+    footerBlock.append(container);
+    if (copyright) {
+        const copyrightBlock = getElement("div", ["left"]);
+        const copyrightContent = getElement("span", ["copyright"], {
+            textContent: copyright,
+        });
+        copyrightBlock.append(copyrightContent);
+        content.append(copyrightBlock);
+    }
+    if (menu) {
+        const menuBlock = getElement("div", ["right"]);
+        const menuNav = getElement("nav", ["footer-menu"]);
+        menuBlock.append(menuNav);
+        const allMenu = menu.map((item) => {
+            return getElement("a", ["footer-link"], {
+                textContent: item.title,
+                href: item.link,
+            });
+        });
+        menuNav.append(...allMenu);
+        content.append(menuBlock);
+    }
 
-	return footerBlock;
+    return footerBlock;
 };
 
 const movieConstructor = (selector, options) => {
-	const app = document.querySelector(selector);
-	app.classList.add("body-app");
+    const app = document.querySelector(selector);
+    app.classList.add("body-app");
 
-	app.style.color = options.fontColor || "";
-	app.style.backgroundColor = options.backgroundColor || "";
-	if (options.subColor) {
-		document.documentElement.style.setProperty("--sub-color", options.subColor);
-	}
+    app.style.color = options.fontColor || "";
+    app.style.backgroundColor = options.backgroundColor || "";
+    if (options.subColor) {
+        document.documentElement.style.setProperty("--sub-color", options.subColor);
+    }
 
-	if (options.favicon) {
-		const index = options.favicon.lastIndexOf(".");
-		const type = options.favicon.substring(index + 1);
-		const favicon = getElement("link", null, {
-			rel: "icon",
-			href: options.favicon,
-			type: "image/" + type === "svg" ? "image/svg-xml" : type
-		});
-		document.head.append(favicon);
-	}
+    if (options.favicon) {
+        const index = options.favicon.lastIndexOf(".");
+        const type = options.favicon.substring(index + 1);
+        const favicon = getElement("link", null, {
+            rel: "icon",
+            href: options.favicon,
+            type: "image/" + type === "svg" ? "image/svg-xml" : type,
+        });
+        document.head.append(favicon);
+    }
 
-	app.style.backgroundImage = options.background ?
-		`url("${options.background}")` : "";
-	document.title = options.title;
+    app.style.backgroundImage = options.background ? `url("${options.background}")` : "";
+    document.title = options.title;
 
-	//заголовок
-	if (options.header) {
-		app.append(createHeader(options));
-	}
-	//контент
-	if (options.main) {
-		app.append(createMain(options));
-	}
-	//футер
-	if (options.footer) {
-		app.append(createFooter(options));
-	}
+    //заголовок
+    if (options.header) {
+        app.append(createHeader(options));
+    }
+    //контент
+    if (options.main) {
+        app.append(createMain(options));
+    }
+    //футер
+    if (options.footer) {
+        app.append(createFooter(options));
+    }
 };
 
-
-movieConstructor(".app", {
-    title: "Ведьмак",
-    background: "witcher/background.jpg",
-    favicon: "witcher/logo.png",
-    fontColor: "#ffffff",
-    backgroundColor: "#141218",
-    subColor: "#9D2929",
-    header: {
-        logo: "witcher/logo.png",
-        social: [
-            {
-                title: "Twitter",
-                link: "https://twitter.com",
-                image: "witcher/social/twitter.svg",
-            },
-            {
-                title: "Instagram",
-                link: "https://instagram.com",
-                image: "witcher/social/instagram.svg",
-            },
-            {
-                title: "Facebok",
-                link: "https://facebook.com",
-                image: "witcher/social/facebook.svg",
-            },
-        ],
-        menu: [
-            {
-                title: "Описание",
-                link: "#",
-            },
-            {
-                title: "Трейлер",
-                link: "#",
-            },
-            {
-                title: "Отзывы",
-                link: "#",
-            },
-        ],
-    },
-    main: {
-        genre: "2019, фэнтези",
-        rating: 8,
-        description: `Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва
-                        путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от
-                        всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.`,
-        trailer: "https://www.youtube.com/watch?v=P0oJqfLzZzQ",
-        slider: [
-            {
-                img: "witcher/series/series-1.jpg",
-                title: "Начало конца",
-                subtitle: "Серия №1",
-            },
-            {
-                img: "witcher/series/series-2.jpg",
-                title: "Четыре марки",
-                subtitle: "Серия №2",
-            },
-            {
-                img: "witcher/series/series-3.jpg",
-                title: "Предательская луна",
-                subtitle: "Серия №3",
-            },
-            {
-                img: "witcher/series/series-4.jpg",
-                title: "Банкеты, ублюдки и похороны",
-                subtitle: "Серия №4",
-            },
-        ],
-    },
-    footer: {
-		copyright: "© 2020 The Witcher. All right reserved.",
-		menu: [
-			{
-				title: "Privacy Policy",
-				link: "#"
-			},
-			{
-				title: "Terms of Service",
-				link: "#"
-			},
-			{
-				title: "Legal",
-				link: "#"
-			}
-		]
-    },
-});
+movieConstructor(".app", configObject);
